9ceb6bd61c8aca75c07ff3f0e6e86299a97db6bf
Yeasin <ar9485727@gmail.com>
2024-06-25 17:03:21 +0600

    middleware added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,21 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export const  middleware = async (request) =>{
-    return NextResponse.next()
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const user = request.cookies.get("next-auth.session-token");
+  const pathname = request.nextUrl.pathname;
+  
+  if (pathname.includes("api")) {
+    return NextResponse.next();
+  }
+  if (!user) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
 }
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/events/:id*",
+};
